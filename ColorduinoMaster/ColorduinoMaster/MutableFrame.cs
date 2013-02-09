85eea65d8f297f950590c954fefd367d9dd9d990
foobert <0x002a@gmail.com>
2013-02-09 11:30:29 +0100

    MutableFrame supports basic alpha blending

--- ColorduinoMaster/ColorduinoMaster/MutableFrame.cs
+++ ColorduinoMaster/ColorduinoMaster/MutableFrame.cs
@@ -11,14 +11,23 @@ namespace ColorduinoMaster
 
 	public class MutableFrame : Frame
 	{
+        private double _alpha;
+
 		public MutableFrame()
 		{
+            _alpha = 1.0;
 		}
 
-		public MutableFrame(Frame frame)
+		public MutableFrame(Frame frame, double alpha)
 			: base(frame.Duration, frame.AllPixels().ToArray())
 		{
+            _alpha = alpha;
 		}
+
+        public MutableFrame(Frame frame)
+            : this(frame, 1.0)
+        {
+        }
 
 		public void SetPixel(int x, int y, int c)
 		{
@@ -34,10 +43,19 @@ namespace ColorduinoMaster
 		}
 
 		public void SetPixel(int x, int y, Color c)
-		{
-			int index = PosToIndex(x, y);
-			_pixels[index] = c;
-		}
+        {
+            int index = PosToIndex(x, y);
+            if (_alpha < 1.0)
+            {
+                var oldColor = _pixels [index];
+                var newColor = Color.FromArgb((oldColor.R + c.R) / 2, (oldColor.G + c.G) / 2, (oldColor.B + c.B) / 2);
+                _pixels[index] = newColor;
+            }
+            else
+            {
+                _pixels[index] = c;
+            }
+        }
 
 		public void LoadBitmap(Bitmap bitmap)
 		{
