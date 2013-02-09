520e791e8af3e7101e3970fc8c7cb34cc9b06550
foobert <0x002a@gmail.com>
2013-02-09 11:29:58 +0100

    Updated font to include dash '-'

--- ColorduinoMaster/ColorduinoMaster/Font.cs
+++ ColorduinoMaster/ColorduinoMaster/Font.cs
@@ -14,7 +14,7 @@ namespace ColorduinoMaster
             '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
             'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-            'U', 'V', 'W', 'X', 'Y', 'Z', '°'
+            'U', 'V', 'W', 'X', 'Y', 'Z', '°', '-'
         };
 
 		public Font(string filename)
